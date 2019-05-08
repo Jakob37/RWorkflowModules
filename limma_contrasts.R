e686d92cdee730e3ba72683a08ebc37024eff875
Jakob Willforss <jakob.willforss@hotmail.com>
2019-05-08 11:41:19 +0200

    Bug fix: Presence/absence checked over all contrasts and not only the first one

--- limma_contrasts.R
+++ limma_contrasts.R
@@ -127,8 +127,8 @@ calculate_presence_absence_tables <- function(ddf, sdf, contrasts, sample_col, c
     presence_absence_list <- list()
     for (contrast_i in seq_len(nrow(split_contrasts_only_name))) {
 
-        samples_high <- ddf %>% filter(UQ(as.name(cond_col)) %in% split_contrasts_only_name[1, 1]) %>% select(sample_col) %>% unlist(use.names=F)
-        samples_low <- ddf %>% filter(UQ(as.name(cond_col)) %in% split_contrasts_only_name[1, 2]) %>% select(sample_col) %>% unlist(use.names=F)
+        samples_high <- ddf %>% filter(UQ(as.name(cond_col)) %in% split_contrasts_only_name[contrast_i, 1]) %>% dplyr::select(sample_col) %>% unlist(use.names=FALSE)
+        samples_low <- ddf %>% filter(UQ(as.name(cond_col)) %in% split_contrasts_only_name[contrast_i, 2]) %>% dplyr::select(sample_col) %>% unlist(use.names=FALSE)
         
         high_has_values <- apply(sdf[, samples_high], 1, function(row) { length(na.omit(row)) })
         low_has_values <- apply(sdf[, samples_low], 1, function(row) { length(na.omit(row)) })
